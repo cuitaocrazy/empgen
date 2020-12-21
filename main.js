2bb7f7977a2a32f3d50d5f40acad11e65db15dbe
scs <scstheone@163.com>
2020-12-21 16:17:34 +0800

    empList

--- main.js
+++ main.js
@@ -8,22 +8,28 @@ function getUsers () {
     enabled: true,
     firstName: l[3].slice(1),
     lastName: l[3][0],
+    email:l[5],
+    realmRoles: (roles.map(v=>v[1]===l[4]&&v[0]).filter(Boolean)).concat("offline_access", "uma_authorization"),
+    groups: [l[6]!==undefined?l[2]+"/"+l[6]:l[2]],
+    clientRoles: {
+      "account": ["view-profile", "manage-account"]
+  },
     credentials: [{
       type: 'password',
       value: 'password'
-    }]
+    }],
+
+
   }))
 }
-
+const roles = [
+  ['assistant', '助理'],
+  ['supervisor', '主管'],
+  ['project_manager', '项目经理'],
+  ['group_leader', '组长'],
+  ['engineer', '工程师']
+]
 function getRoles () {
-  const roles = [
-    ['assistant', '助理'],
-    ['supervisor', '主管'],
-    ['project_manager', '项目经理'],
-    ['group_leader', '组长'],
-    ['engineer', '工程师']
-  ]
-
   return roles.map(r => ({ name: r[0], description: r[1] }))
 }
 
@@ -34,12 +40,16 @@ function getGroup () {
       path: '/项目二部',
       subGroups: [
         {
-          name: '二组',
-          path: '/项目二部/一组'
+          name: '苏州组',
+          path: '/项目二部/苏州组'
         },
         {
-          name: '一组',
-          path: '/项目二部/二组'
+          name: '创新组',
+          path: '/项目二部/创新组'
+        },
+        {
+          name: '特色组',
+          path: '/项目二部/特色组'
         }
       ]
     }
